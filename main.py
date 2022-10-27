ef655c95b917c54a2a962347c061b3b6c385179a
Millard <millard2santos@gmail.com>
2022-10-27 20:03:01 +0200

    Curso de Python Day 1

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Bienvenido al generador de nombres para bandas!!")
+
+x = input("Cual es tu animal favorito en plural? : ")
+y = input("En que ciudad te criaste? : ")
+print(f"El nombre de tu banda es : Los/Las {x} de {y}")
